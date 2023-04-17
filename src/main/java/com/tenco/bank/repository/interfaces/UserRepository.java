5c6fa5b97b31295796ebc72f0300789c2f7ccb1a
mejjjung <zalwjdz29@naver.com>
2023-04-17 12:31:04 +0900

    mybatis 설정 및 기본 화면 구성 1

---
+++ src/main/java/com/tenco/bank/repository/interfaces/UserRepository.java
@@ -0,0 +1,17 @@
+package com.tenco.bank.repository.interfaces;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.tenco.bank.repository.model.User;
+
+@Mapper // MyBatis 의존 설정 함
+public interface UserRepository {
+	
+	public int insert(User user);
+	public int updateById(User user);
+	public int deleteById(Integer principal);
+	public User findById(Integer id);
+	public List<User> findAll();
+}
